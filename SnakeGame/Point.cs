2eb8dba8b1009893ba72197033738a44ef8c1532
Ivan Danylevych <3wander3@gmail.com>
2020-04-23 09:27:34 +0300

    Dots are colored

---
+++ SnakeGame/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private char sym;
+        private ConsoleColor color;
+
+        public Point(int x, int y, char sym, ConsoleColor color)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+            this.color = color;
+        }
+
+        public void Draw()
+        {
+            Console.ForegroundColor = color;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+    }
+}
--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -6,25 +6,14 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            Point p1 = new Point(1, 3, '*', ConsoleColor.Red);
+            p1.Draw();
 
-            Draw(x1, y1, sym1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);
+            Point p2 = new Point(4, 5, '#', ConsoleColor.Green);
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(sym);
+            Console.ResetColor();
         }
     }
 }
